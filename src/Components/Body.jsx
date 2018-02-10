e862f39342339170ebdbf60c3be4c44f7f07fbb1
Steven Lee <info@steven-lee.com>
2018-02-10 10:29:19 -0800

    google requests coming in

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -9,12 +9,16 @@ export default class Body extends Component {
     super(props);
     this.state = {
       city: "",
-      sites: {},
+      sights: {},
       bites: {}
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  // updateSights(response) {
+  //   this.setState({ sights: response.data.results });
+  // }
 
   handleChange(event) {
     this.setState({ city: event.target.value });
@@ -22,22 +26,48 @@ export default class Body extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    //do the api calls here
-    const config = {
-      headers: {
-        Authorization:
-          "Bearer Y4NDfjO891ERjcyaIa2z7Q2pjAjqtBcYoU7XLWpXZotV2EtOd3ZH5CrsCCrSgLEJN6FkHZEXbfCJdEWBuyVDY7u6KyvL0osIsgLPdjVPKX4aIgdF8xKYWecaj_J9WnYx"
-      }
-    };
+    // do the api calls here
+
+    //   axios
+    //     .get(
+    //       "https://api.yelp.com/v3/businesses/search?location=%" +
+    //         this.state.city +
+    //         "&categories=Food&sort_by=rating",
+    //       {
+    //         headers: {
+    //           Authorization:
+    //             "Bearer Y4NDfjO891ERjcyaIa2z7Q2pjAjqtBcYoU7XLWpXZotV2EtOd3ZH5CrsCCrSgLEJN6FkHZEXbfCJdEWBuyVDY7u6KyvL0osIsgLPdjVPKX4aIgdF8xKYWecaj_J9WnYx"
+    //         }
+    //       }
+    //     )
+    //     .then(function(response) {
+    //       console.log("response", response);
+    //     })
+    //     .catch(function(err) {
+    //       console.log("err", err);
+    //     });
+    // }
     axios
       .get(
-        "https://api.yelp.com/v3/businesses/search?location=%" +
+        "https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+in+" +
           this.state.city +
-          "&categories=Food&sort_by=rating",
-        config
+          "&key=AIzaSyCX9EPP7EsRb9xbWmupveSgdW2l_AJ9JGU",
+        {
+          mode: "no-cors",
+          header: {
+            "Access-Control-Allow-Orgin": "http://localhost:3000",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers":
+              "Origin, X-Requested-With, Content-Type, Accept"
+          },
+          withCredentials: true,
+          credentials: "same-origin"
+        }
       )
       .then(function(response) {
         console.log("response", response);
+        this.setState({ sights: response.data.results });
+        // this.updateSights(response);
       })
       .catch(function(err) {
         console.log("err", err);
