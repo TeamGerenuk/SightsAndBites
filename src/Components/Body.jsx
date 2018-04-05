ce5267ed85390c7fb09fd9d29f67ab35ab803454
scottyb <scottyb@scottybs-MacBook.local>
2018-04-05 11:34:25 -0700

    updating

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -28,51 +28,51 @@ export default class Body extends Component {
     event.preventDefault();
     // do the api calls here
 
-    //   axios
-    //     .get(
-    //       "https://api.yelp.com/v3/businesses/search?location=%" +
-    //         this.state.city +
-    //         "&categories=Food&sort_by=rating",
-    //       {
-    //         headers: {
-    //           Authorization:
-    //             "Bearer Y4NDfjO891ERjcyaIa2z7Q2pjAjqtBcYoU7XLWpXZotV2EtOd3ZH5CrsCCrSgLEJN6FkHZEXbfCJdEWBuyVDY7u6KyvL0osIsgLPdjVPKX4aIgdF8xKYWecaj_J9WnYx"
-    //         }
-    //       }
-    //     )
-    //     .then(function(response) {
-    //       console.log("response", response);
-    //     })
-    //     .catch(function(err) {
-    //       console.log("err", err);
-    //     });
-    // }
-    axios
-      .get(
-        "https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+in+" +
-          this.state.city +
-          "&key=AIzaSyCX9EPP7EsRb9xbWmupveSgdW2l_AJ9JGU",
-        {
-          mode: "no-cors",
-          header: {
-            "Access-Control-Allow-Orgin": "http://localhost:3000",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Headers":
-              "Origin, X-Requested-With, Content-Type, Accept"
-          },
-          withCredentials: true,
-          credentials: "same-origin"
-        }
-      )
-      .then(function(response) {
-        console.log("response", response);
-        this.setState({ sights: response.data.results });
-        // this.updateSights(response);
-      })
-      .catch(function(err) {
-        console.log("err", err);
-      });
-  }
+      axios
+        .post(
+          "https://api.yelp.com/v3/businesses/search?location=%" +
+            this.state.city +
+            "&categories=Food&sort_by=rating",
+          {
+            headers: {
+              Authorization:
+                "Bearer Y4NDfjO891ERjcyaIa2z7Q2pjAjqtBcYoU7XLWpXZotV2EtOd3ZH5CrsCCrSgLEJN6FkHZEXbfCJdEWBuyVDY7u6KyvL0osIsgLPdjVPKX4aIgdF8xKYWecaj_J9WnYx"
+            }
+          }
+        )
+        .then(function(response) {
+          console.log("response", response);
+        })
+        .catch(function(err) {
+          console.log("err", err);
+        });
+    }
+  //   axios
+  //     .get(
+  //       "https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+in+" +
+  //         this.state.city +
+  //         "&key=AIzaSyCX9EPP7EsRb9xbWmupveSgdW2l_AJ9JGU",
+  //       {
+  //         mode: "no-cors",
+  //         header: {
+  //           "Access-Control-Allow-Orgin": "http://localhost:3000",
+  //           "Content-Type": "application/json",
+  //           "Access-Control-Allow-Headers":
+  //             "Origin, X-Requested-With, Content-Type, Accept"
+  //         },
+  //         withCredentials: true,
+  //         credentials: "same-origin"
+  //       }
+  //     )
+  //     .then(function(response) {
+  //       console.log("response", response);
+  //       this.setState({ sights: response.data.results });
+  //       // this.updateSights(response);
+  //     })
+  //     .catch(function(err) {
+  //       console.log("err", err);
+  //     });
+  // }
 
   render() {
     return (
